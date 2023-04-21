2366ab43b4af6c91acb9c343b03611fa8fce140e
Sujal Dantluri <sujaldantluri@gmail.com>
2023-04-20 20:00:34 -0500

    Create django server

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9*6(v9z1+o$44*t4+p-vm!og_%@qxd4id&v7cle5&q=rg5%$s#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'chicken196',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
